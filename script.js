79d9ccf1c71d176af9a25e94f9460ffc13c137a1
AndrewGavin304 <AndrewGavin304@gmail.com>
2022-07-07 11:03:33 -0400

    Make button container; refactor playRound to take button input

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ function computerPlay(){
 }
 
 function userPlay(){
-    let choice = prompt("Choose rock, paper, or scissors.").toLowerCase();
+    let choice = target.id;
+    console.log(choice)
     return choice;
 }
 
-function playRound(playerSelection, computerSelection){
+function playRound(playerSelection){
+    let computerSelection = computerPlay();
     if (playerSelection == computerSelection){
         console.log(`Tie!  You both chose ${playerSelection}`);
         return "tie";
@@ -23,26 +25,35 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
-function game(){
-    let userWins = 0;
-    let cpuWins = 0;
-    let draws = 0;
-    for (let i = 0; i < 5; i++){
-        let playerSelection = userPlay();
-        let computerSelection = computerPlay();
-        let result = playRound(playerSelection, computerSelection);
-        if (result == "win") {
-            userWins++
-        }
-        else if (result == "loss") {
-            cpuWins++
-        }
-        else {
-            draws++
-        }
-        console.log(`Current score is ${userWins}-${cpuWins}-${draws}`);
-    }
-}
+const buttonContainer = document.getElementById("buttonContainer");
 
-game();
+buttonContainer.addEventListener('click', (e) => {
+    if (e.target.id == "buttonContainer"){
+    return false;
+    }
+    playRound(e.target.id)
+})
+
+
+// function game(){
+//     let userWins = 0;
+//     let cpuWins = 0;
+//     let draws = 0;
+//     for (let i = 0; i < 5; i++){
+//         let playerSelection = userPlay();
+//         let computerSelection = computerPlay();
+//         let result = playRound(playerSelection, computerSelection);
+//         if (result == "win") {
+//             userWins++
+//         }
+//         else if (result == "loss") {
+//             cpuWins++
+//         }
+//         else {
+//             draws++
+//         }
+//         console.log(`Current score is ${userWins}-${cpuWins}-${draws}`);
+//     }
+// }
+
 
